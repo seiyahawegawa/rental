3ff1ac428ed87bf42e8d21843f18509fdf69236c
seiyahawegawa <h-ayiesn_n-1270@docomo.ne.jp>
2020-09-19 10:37:37 +0900

    commit

---
+++ app/controllers/womans_controller.rb
@@ -0,0 +1,4 @@
+class WomansController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'womans/index'
   devise_for :users
   root "clothesinformations#index"
+  resources :clothesinformations, only: :index
   resources :users, only: [:edit, :update]
+  resources :womans, only: :index
 end
